6a59c43bbafb0b6033fd610edf0356ce73719a24
alexandre-magnier-dv <alexandre.magnier@digitalvirgo.com>
2025-11-21 18:13:29 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,18 @@
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/**/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    'dark',
+    {
+      pattern: /^(bg|text|border|from|via|to)-(slate|gray|purple|cyan|blue|yellow|green|red)-(50|100|200|300|400|500|600|700|800|900)$/,
+      variants: ['dark', 'hover', 'dark:hover'],
+    },
+  ],
 };
